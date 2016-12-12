eea5fc7210d1850678ce0b7b1a0f4286592a0b9f
Lorenzo Sciandra <sciandra@leva.io>
2016-12-12 10:50:14 +0100

    modifying the logger setup to be more Docker compliant

---
+++ chat/config/initializers/logger.rb
@@ -0,0 +1,4 @@
+logger           = ActiveSupport::Logger.new(STDOUT)
+logger.formatter = config.log_formatter
+config.log_tags  = [:subdomain, :uuid]
+config.logger    = ActiveSupport::TaggedLogging.new(logger)
