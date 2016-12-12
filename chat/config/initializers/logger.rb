7fe77a4911ac94adc1c8ba618f8565fe65a16a6a
Lorenzo Sciandra <sciandra@leva.io>
2016-12-12 10:56:43 +0100

    I need to use the orat generator

--- chat/config/initializers/logger.rb
+++
@@ -1,4 +0,0 @@
-logger           = ActiveSupport::Logger.new(STDOUT)
-logger.formatter = config.log_formatter
-config.log_tags  = [:subdomain, :uuid]
-config.logger    = ActiveSupport::TaggedLogging.new(logger)
