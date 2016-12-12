2e295a2d3343924620021bc17fac0afabd0e8771
Lorenzo Sciandra <sciandra@leva.io>
2016-12-12 13:01:25 +0100

    Adding some manipulation methods and giving some jQuery control over UI

--- dockerized_chat/config/routes.rb
+++ dockerized_chat/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'pages#home'
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+  end
 end
